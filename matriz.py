2a168a801288d10815f2b46300245a2626adebb7
Antonio Adelino <antonio.asn03@gmail.com>
2019-01-16 16:05:47 -0300

    Ajustes na classe matriz

--- matriz.py
+++ matriz.py
@@ -2,19 +2,33 @@ class Matriz:
 
     def __init__(self, linhas, colunas):
         self.elementos = self.gerarMatriz(linhas, colunas)
-
+        self.linha = linhas
+        self.coluna = colunas
 
     def gerarMatriz(self, linhas, colunas):
         vetorExterno = []
         for k in range(linhas):
+            vetorInterno = []
             for i in range(colunas):
-                    vetorInterno = []
-                    vetorInterno.append(-1)
-                    vetorExterno.append(vetorInterno)
+                vetorInterno.append(-1)
+            vetorExterno.append(vetorInterno)
         return vetorExterno
 
-
     def inserirElemento(self, linha, coluna, ponto):
-        self.elementos[linha].insert(coluna, ponto)
+        self.elementos[linha][coluna] = ponto
         ponto.setLinha(linha)
         ponto.setColuna(coluna)
+
+    def mostrarMatriz(self, matriz, linhas, colunas):
+        matrizP = ''
+        for k in range(linhas):
+            linha = ""
+            for i in range(colunas):
+                elemento = matriz.elementos[k][i]
+                if elemento == -1:
+                    linha += " -1 "
+                else:
+                    linha += "  "+elemento.nome+" "
+            matrizP += linha+"\n"
+
+        print(matrizP)
--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 from matriz import Matriz
 from ponto import Ponto
-import caixeiroViajante
+from caixeiroViajante import *
 
 matriz = Matriz(5,5)
 
@@ -12,13 +12,25 @@ ponto5 = Ponto('E')
 ponto6 = Ponto('F')
 ponto7 = Ponto('G')
 
-matriz.inserirElemento(0,1,ponto1)
-matriz.inserirElemento(2,2,ponto2)
-matriz.inserirElemento(2,3,ponto3)
-matriz.inserirElemento(3,3,ponto4)
-matriz.inserirElemento(3,4,ponto5)
-matriz.inserirElemento(4,2,ponto6)
-matriz.inserirElemento(3,0,ponto7)
 
-resultado = caixeiroViajante(matriz, ponto2)
-mostrarResultado(resultado)
+matriz.inserirElemento(0,1,ponto1)
+print(matriz.elementos[0][1].nome)
+matriz.inserirElemento(2,2,ponto2)
+print(matriz.elementos[2][2].nome)
+matriz.inserirElemento(2,3,ponto3)
+print(matriz.elementos[2][3].nome)
+matriz.inserirElemento(3,3,ponto4)
+print(matriz.elementos[3][3].nome)
+matriz.inserirElemento(3,4,ponto5)
+print(matriz.elementos[3][4].nome)
+matriz.inserirElemento(4,2,ponto6)
+print(matriz.elementos[4][2].nome)
+matriz.inserirElemento(3,0,ponto7)
+print(matriz.elementos[3][0].nome)
+print(" ")
+
+matriz.mostrarMatriz(matriz,5,5)
+#
+# resultado = []
+# resultado = caixeiroViajante(matriz, ponto2)
+# mostrarResultado(resultado)
