7d8e15fa9f80b266726500e22d7f08d8b83e1ad4
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2019-01-21 04:30:53 -0300

    Algoritmo Backtracking

---
+++ Algoritmo Backtracking/caixeiroViajante.py
@@ -0,0 +1,67 @@
+from matriz import Matriz
+from ponto import Ponto
+
+def resgatarPontos(matriz):
+    retorno = []
+    for k in range(matriz.linha):
+        for i in range(matriz.coluna):
+                if matriz.elementos[i][k] != -1:
+                    retorno.append(matriz.elementos[i][k])
+    return retorno
+
+def eliminarIntersecao(lista1, lista2):
+    for i in range(len(lista2)):
+        if lista2[i] in lista1:
+            lista1.remove(lista2[i])
+    return lista1
+
+
+def caixeiroViajanteBT(matriz):
+    pontos = resgatarPontos(matriz)
+    verificados = []
+    caminho = []
+
+    caminho.append(pontos[0])
+    caminho[0].verificado = True
+    verificados.append(pontos[0])
+    # for k in range(len(pontos))
+
+    i = 0
+    k = 1
+
+    while len(caminho) != len(pontos) and k < len(pontos):
+        i += 1
+        caminho.append(pontos[k])
+
+        print(caminho[i-1].nome + ' ' +str(caminho[i-1].linha))
+        print(pontos[k].nome+ ' ' +str(pontos[k].linha))
+
+        if caminho[i-1].linha > pontos[k].linha and (caminho[i-1].verificado == False):
+
+            caminho[i].verificado = True
+            verificados.append(caminho[i])
+            caminho.remove(caminho[i-1])
+            k += 1
+            i -= 1
+
+
+
+        else:
+
+            if k < len(pontos):
+                k += 1
+
+    pontos = eliminarIntersecao(pontos, caminho)
+    pontos.reverse()
+
+    for k in range(len(pontos)):
+        caminho.append(pontos[k])
+
+    return caminho
+
+def printArray(array):
+    valor = []
+    for k in range(len(array)):
+        valor.append(array[k].nome)
+        # valor.append(array[k])
+    print(valor)
---
+++ Algoritmo Backtracking/teste.py
@@ -0,0 +1,45 @@
+from matriz import Matriz
+from ponto import Ponto
+from caixeiroViajante import *
+from datetime import datetime
+
+matriz = Matriz(9)
+
+ponto1 = Ponto('A')
+ponto2 = Ponto('B')
+ponto3 = Ponto('C')
+ponto4 = Ponto('D')
+ponto5 = Ponto('E')
+ponto6 = Ponto('F')
+ponto7 = Ponto('G')
+
+
+matriz.inserirElemento(0,0,ponto1)
+matriz.inserirElemento(6,1,ponto2)
+matriz.inserirElemento(3,2,ponto3)
+matriz.inserirElemento(2,5,ponto4)
+matriz.inserirElemento(5,6,ponto5)
+matriz.inserirElemento(1,7,ponto6)
+matriz.inserirElemento(4,8,ponto7)
+
+print("Problema bitônico euclidiano do caixeiro-viajante:")
+matriz.mostrarMatriz(matriz) #Print da matriz
+print(" ")
+
+# Registra o momento antes da execução do algoritmo
+t0 = datetime.now()
+
+resp = caixeiroViajanteBT(matriz) #Realiza o caminho bitônico
+
+# Registra o momento apos a execução do algoritmo
+t1 = datetime.now()
+
+# Calcula a o tempo de execucao em milissegundos da operacao
+diff = t1 - t0
+tempo = (diff.total_seconds() * 1000)
+
+printArray(resp) #Print do caminho
+print(" ")
+
+# Exibe resultado do teste
+print("Tempo de execução do Algoritmo Backtracking: " + str(tempo) + " ms")
